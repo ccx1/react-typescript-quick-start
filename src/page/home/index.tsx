6bd690f692009e26baefa3f504863b77224a4984
v_chicunxiang <v_chicunxiang@baidu.com>
2020-05-06 10:19:08 +0800

    修改type

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import {connect} from "react-redux";
+import {connect, DispatchProp} from "react-redux";
 import {IPageInfo} from "@/reducer/BasicReducer";
 import {Link, RouteComponentProps} from "react-router-dom";
-import {Dispatch} from "redux";
 
 interface IHomeState {
 
 }
 
-interface IHomeProps extends RouteComponentProps {
+type IHomeBaseProps = RouteComponentProps & DispatchProp;
+
+interface IHomeProps extends IHomeBaseProps {
     pageInfo: IPageInfo,
-    dispatch:Dispatch
 }
 
 class Home extends React.Component<IHomeProps, IHomeState> {
